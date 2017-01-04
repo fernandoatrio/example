915f0017796477dfc7c475dc699ddbdcb7fa1620
Fernando Atrio <fernandoatrio@gmail.com>
2017-01-04 09:41:38 -0300

    Trivial refactor

--- src/main/java/com/demo/persistence/user/UserSpecifications.java
+++ src/main/java/com/demo/persistence/user/UserSpecifications.java
@@ -4,13 +4,9 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class UserSpecifications {
 	public static Specification<User> hasEmail(String email) {
-		return (root, query, cb) -> {
-			return cb.equal(root.get("email"), email);
-		};
+		return (root, query, cb) -> cb.equal(root.get("email"), email);
 	}
 	public static Specification<User> hasLastName(String lastName) {
-		return (root, query, cb) -> {
-			return cb.equal(root.get("lastName"), lastName);
-		};
+		return (root, query, cb) -> cb.equal(root.get("lastName"), lastName);
 	}
 }
